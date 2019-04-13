cddbfbec0a79619a18fc364d4af6442fe410b2a4
kaitohattori <kaichan394@gmail.com>
2019-04-13 16:56:15 +0900

    Updated index.html

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import jsonify
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./template')
 
 @app.route('/')
 def index():
