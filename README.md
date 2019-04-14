1ef6b79566968942d9e33c143c7599894bfaa994
kaitohattori <kaichan394@gmail.com>
2019-04-14 19:25:46 +0900

    Changed API function, weather to calculator

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Heroku上でFlaskを動かしてWeather情報を取得します
+# Heroku上でFlaskを動かして計算機APIを作る
+
+## 作ったもの
+
+APIはこちら
+
+[https://kaitohattori-api-calculator.herokuapp.com/](https://kaitohattori-api-calculator.herokuapp.com/)
+
+## コマンドリスト
+
+- /sum
+- /average
+- /max
+- /min
+
+## JSONフォーマット
+
+``` POSTするときのフォーマット
+{
+    'values': [1, 2, 3]
+}
+```
---
+++ app.py
@@ -0,0 +1,100 @@
+import os
+
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+from flask import request
+
+app = Flask(__name__, template_folder='./template')
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+@app.route('/sum', methods=['POST'])
+def cal_sum():
+    request_json = request.json
+
+    required = ['values']
+    if not all(k in request_json for k in required):
+        return 'Missing values', 400
+
+    values = request_json['values']
+
+    val_sum = 0
+    for value in values:
+        val_sum += value
+
+    response = {
+        'values': values,
+        'sum': val_sum
+    }
+    return jsonify(response), 201
+
+@app.route('/average', methods=['POST'])
+def cal_average():
+    request_json = request.json
+
+    required = ['values']
+    if not all(k in request_json for k in required):
+        return 'Missing values', 400
+
+    values = request_json['values']
+
+    val_sum = 0
+    for value in values:
+        val_sum += value
+    
+    val_average = val_sum / len(values)
+
+    response = {
+        'values': values,
+        'average': val_average
+    }
+    return jsonify(response), 201
+
+@app.route('/max', methods=['POST'])
+def cal_max():
+    request_json = request.json
+
+    required = ['values']
+    if not all(k in request_json for k in required):
+        return 'Missing values', 400
+
+    values = request_json['values']
+
+    val_max = values[0]
+    for value in values:
+        if value > val_max:
+            val_max = value
+
+    response = {
+        'values': values,
+        'max': val_max
+    }
+    return jsonify(response), 201
+
+@app.route('/min', methods=['POST'])
+def cal_min():
+    request_json = request.json
+
+    required = ['values']
+    if not all(k in request_json for k in required):
+        return 'Missing values', 400
+
+    values = request_json['values']
+
+    val_min = values[0]
+    for value in values:
+        if value < val_min:
+            val_min = value
+
+    response = {
+        'values': values,
+        'min': val_min
+    }
+    return jsonify(response), 201
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
